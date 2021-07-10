25d6cf09721395ecaa0afa443718a07fa6f2e00c
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2021-07-10 18:44:35 +0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6d583c24104b8fe12fa9433c9342e5ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '06c33ab4-706f-4482-b4fb-53b0516edd0b'
+};
