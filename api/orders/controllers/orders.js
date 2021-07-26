feaede369176af9c4cf1e58b0fcb859826a50faf
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2021-07-26 13:53:49 +0600

    Corrected import

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -1,10 +1,5 @@
 "use strict";
-const { sanitizeEntity } = require("strapi-utils");
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   // Create order associated with user
